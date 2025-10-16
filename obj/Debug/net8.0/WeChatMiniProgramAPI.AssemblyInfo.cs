1c503b0a1d1eb3d62ecb085f2f63f57189116f8c
qsswgl <qsswgl@users.noreply.github.com>
2025-10-16 10:25:57 +0800

    配置8092端口访问和SSH over HTTPS推送
    - 移除8091端口映射，只保留8092端口HTTPS访问
    - 配置数据持久化存储到/data/wechat-uploadall
    - 修复权限问题确保容器可以写入文件
    - 配置SSH over HTTPS方式推送到GitHub

--- obj/Debug/net8.0/WeChatMiniProgramAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/WeChatMiniProgramAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeChatMiniProgramAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+246d957449528dae2c82f0cd95af5b5758f8761d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a99f804acc87b2b78ede2b27c37a81bfc0617484")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeChatMiniProgramAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeChatMiniProgramAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
